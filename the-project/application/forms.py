166d3bca5c1da156cd9ababa65b4673885197bf7
v-za <za.vinit@gmail.com>
2019-10-27 20:29:27 -0500

    m

--- the-project/application/forms.py
+++ the-project/application/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField, SubmitField, BooleanField, IntegerField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from application.models import Pet, User, Product
 
 #inherting FlaskForm class
 
@@ -11,6 +12,11 @@ class UserRegistrationForm(FlaskForm):
     password = PasswordField('Password',validators=[DataRequired(message="Please Enter a Password")])
     confirm_password = PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class UserLoginForm(FlaskForm):
